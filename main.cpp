dc31b36d4db6c9ee66e9893044952b7e7e306c54
ajmoebius <ajmoebius@ecc-jaguar.csuchico.edu>
2018-09-19 16:15:45 -0700

    Added user input First name only

--- main.cpp
+++ main.cpp
@@ -7,9 +7,13 @@ using namespace std;
 int main()
 {
   //declare variables
-
+  string first = "";
+  string last = "";
+  
   //get user input
-
+  cout<<"Enter first name."<<endl;
+  cin>>first;
+  
   //tell fortune
 
   return 0;
