e48aab1f9c20280b1115b71d57bbd951ef78d6ff
ajmoebius <ajmoebius@ecc-jaguar.csuchico.edu>
2018-09-19 16:43:03 -0700

    Completed

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors: Aaron Moebius
 #include<iostream>
 #include<string>
 
@@ -7,9 +7,9 @@ using namespace std;
 int main()
 {
   //declare variables
-  string first = "";
-  string last = "";
-  
+  string first;
+  string last;
+  int lucky;
   //get user input
   cout<<"Enter first name."<<endl;
   cin>>first;
@@ -18,8 +18,10 @@ int main()
   
   cout<<"Welcome, "<<first[0]<<"."<<last[0]<<"., here is your fortune..."<<endl; 
   
-
   //tell fortune
+  lucky = first.length();
+  
+  cout<<"Your lucky number is "<<lucky<<"."<<endl;
 
   return 0;
 }
